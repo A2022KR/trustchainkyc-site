9f67e45a4342f433e933a2f69923069b0cf1a0c3
A2022KR <ahmed.hamza76@icloud.com>
2025-09-06 00:41:59 +0300

    CSP: allow inline scripts, keep no-eval

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ geolocation=()" },
 
 const csp = `
   default-src 'self';
-  script-src 'self';
+  script-src 'self' 'unsafe-inline';;
   style-src 'self' 'unsafe-inline';
   img-src 'self' data: blob:;
   font-src 'self' data:;
